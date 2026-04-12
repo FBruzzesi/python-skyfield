b1239240b4fce8b090c6ca82bc18072c72f8e7a1
FBruzzesi <francesco.bruzzesi.93@gmail.com>
2026-04-12 19:35:15 +0200

    constants

---
+++ skyfield/constants.pyi
@@ -0,0 +1,30 @@
+# Definitions.
+AU_M: int
+AU_KM: float
+ASEC360: float
+DAY_S: float
+
+# Angles.
+ASEC2RAD: float
+DEG2RAD: float
+RAD2DEG: float
+pi: float
+tau: float
+
+# Physics.
+C: float
+GM_SUN_Pitjeva_2005_km3_s2: int
+
+# Earth and its orbit.
+ANGVEL: float
+ERAD: float
+IERS_2010_INVERSE_EARTH_FLATTENING: float
+
+# Heliocentric gravitational constant in meters^3 / second^2, from DE-405.
+GS: float
+
+# Time.
+T0: float
+B1950: float
+
+C_AUDAY: float
